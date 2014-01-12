71ff8e1d92c5511e322934f079e407a785746f02
Ilja Iwas <ilja@iwascoding.com>
2014-01-12 18:36:34 +0100

    proof-of-concept for CocoaPodsIntegration

---
+++ examples/CocoaPodsTest.m
@@ -0,0 +1,34 @@
+#!/usr/bin/env /usr/bin/objc-run
+
+/*
+podfile-start
+platform :osx, '10.9'
+pod 'Barista'
+podfile-end
+*/
+
+#import <Foundation/Foundation.h>
+#import "Barista.h"
+#import "BARRouter.h"
+
+int main(int argc, const char * argv[])
+{
+	@autoreleasepool
+	{
+		BARServer *server = [BARServer serverWithPort:4242];
+		BARRouter *router = [[BARRouter alloc] init];
+		[router addRoute:@"/" forHTTPMethod:@"GET" handler:^BOOL(BARConnection *connection, BARRequest *request, NSDictionary *parameters) {
+			BARResponse *response = [[BARResponse alloc] init];
+			response.responseData = [@"Hello world" dataUsingEncoding:NSUTF8StringEncoding];
+			response.statusCode = 200;
+			[connection sendResponse:response];
+			return YES;
+		}];
+		[server addGlobalMiddleware:router];
+		[server startListening];
+		while(YES){
+			[[NSRunLoop mainRunLoop] run];
+		}
+	}
+    return 0;
+}
